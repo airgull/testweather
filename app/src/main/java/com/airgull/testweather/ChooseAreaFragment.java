c96b4f0816667326da4f9a96b0345df8374f6ebd
airgull <airgull@sina.com>
2017-06-10 16:45:11 +0800

    Modified listview adapter's bug.

--- app/src/main/java/com/airgull/testweather/ChooseAreaFragment.java
+++ app/src/main/java/com/airgull/testweather/ChooseAreaFragment.java
@@ -1,9 +1,10 @@
 package com.airgull.testweather;
 
-import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -58,27 +59,29 @@ public class ChooseAreaFragment extends Fragment {
     private int currentLevel;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.choose_area, container, false);
         titleText = (TextView) view.findViewById(R.id.title_text);
         backButton = (Button) view.findViewById(R.id.back_button);
         listView = (ListView) view.findViewById(R.id.list_view);
         adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, dataList);
+        listView.setAdapter(adapter);
         return view;
     }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        queryProvinces();
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int postion, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 if (currentLevel == LEVEL_PROVINCE) {
-                    selectedProvince = provinceList.get(postion);
+                    selectedProvince = provinceList.get(position);
                     queryCities();
                 } else if (currentLevel == LEVEL_CITY) {
-                    selectedCity = cityList.get(postion);
+                    selectedCity = cityList.get(position);
                     queryCounties();
                 }
             }
@@ -94,8 +97,7 @@ public class ChooseAreaFragment extends Fragment {
                 }
             }
         });
-        Log.d("airgull", "onActivityCreated: aaaaa");
-        queryProvinces();
+
     }
 
     /**
@@ -115,8 +117,6 @@ public class ChooseAreaFragment extends Fragment {
             currentLevel = LEVEL_PROVINCE;
         } else {
             String address = "http://guolin.tech/api/china";
-            //Log.d(TAG, "queryProvinces: Get " + address);
-
             queryFromServer(address, "province");
         }
     }
