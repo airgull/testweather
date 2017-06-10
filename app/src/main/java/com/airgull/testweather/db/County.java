dcccf7ae3a43983958b5f59136025522d4dc4b4b
airgull <airgull@sina.com>
2017-06-10 09:51:07 +0800

    Create database and data table...

---
+++ app/src/main/java/com/airgull/testweather/db/County.java
@@ -0,0 +1,46 @@
+package com.airgull.testweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by airgull2017 on 2017/6/10.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
